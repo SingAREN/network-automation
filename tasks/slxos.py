49257dbf652c3ab93a3e0e300e3bb00f083a1fde
Simon Green <simonpetergreen@singaren.net.sg>
2026-06-29 20:38:33 +0800

    Update slxos.py

--- tasks/slxos.py
+++ tasks/slxos.py
@@ -1,7 +1,19 @@
 import getpass
 from ncclient import manager
+from ncclient.xml_ import to_ele
+from lxml import etree
 
 
+def edit_config_from_payload(payload):
+    return m.edit_config(target='running', config=payload)
+	
+def get_config_from_filter(payload):
+    return m.get_config(source='running', filter=('subtree', payload))
+	
+def custom_rpc_get_output(payload):
+    return m.dispatch(to_ele(payload))
+
+      
 secret_password = getpass.getpass(prompt="Enter device password: ")
 
 with manager.connect(
