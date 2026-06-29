2c8f38d6d02624aeca61d1b6e6a2515362af1666
Simon Green <simonpetergreen@singaren.net.sg>
2026-06-29 17:56:25 +0800

    Create edit_config.py

---
+++ library/slx-os/edit_config.py
@@ -0,0 +1,20 @@
+from ncclient import manager
+from ncclient.xml_ import to_ele
+from lxml import etree
+
+
+def edit_config_from_payload(payload):
+    return m.edit_config(target='running', config=payload)
+    
+def set_vlan_id(vlan_id, vlan_name):
+    payload = f"""
+    <config xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+        <interface-vlan xmlns="urn:brocade.com:mgmt:brocade-interface">
+            <vlan>
+                <name>{vlan_id}</name>
+                <vlan-name>{vlan_name}</vlan-name>
+            </vlan>
+        </interface-vlan>
+    </config>
+    """
+    return payload
